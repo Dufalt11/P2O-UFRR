44dd34b1b32a6f96d70d81bafacbeb2861c6b05a
Dufalt11 <dufalt11@gmail.com>
2026-08-06 16:27:05 -0400

    ativade de p2o carro

---
+++ Carro/src/main/java/com/mycompany/carro/Carro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.carro;
+
+/**
+ *
+ * @author dufal
+ */
+public class Carro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
